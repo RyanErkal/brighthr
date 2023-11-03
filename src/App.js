d40ebb627888f8b97e34ca5cb82198a0ca7bada6
Ryan Erkal <ryanerkal@yahoo.ie>
2023-11-03 06:09:53 +0000

    files displaying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./assets/data.json";
+import "./App.css";
+
+function getFiles(data) {
+	const files = data.map((file) => {
+		if (file.type === "folder") {
+			return (
+				<p className="folder">
+					./{file.name}
+					<span className="tab">{getFiles(file.files)}</span>
+				</p>
+			);
+		} else {
+			return (
+				<p>
+					{file.name}.{file.type}
+				</p>
+			);
+		}
+	});
+	return files;
+}
+
+const mappedFiles = getFiles(data);
+
+console.log(data);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Bright HR</h1>
+			<select name="select" id="select">
+				<option value="all">All</option>
+				<option value="active">Active</option>
+				<option value="inactive">Inactive</option>
+			</select>
+			<pre>{mappedFiles}</pre>
+		</div>
+	);
 }
 
 export default App;
