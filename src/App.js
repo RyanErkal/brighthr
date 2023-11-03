bc837d8c14d1016f2af8e7f7ca3f6e824e95dec5
Ryan Erkal <ryanerkal@yahoo.ie>
2023-11-03 10:07:22 +0000

    sort & sort order

--- src/App.js
+++ src/App.js
@@ -1,40 +1,75 @@
-import data from "./assets/data.json";
+import { useState, useLayoutEffect } from "react";
+import JSONdata from "./assets/data.json";
 import "./App.css";
 
-function getFiles(data) {
-	const files = data.map((file) => {
-		if (file.type === "folder") {
-			return (
-				<p className="folder">
-					./{file.name}
-					<span className="tab">{getFiles(file.files)}</span>
-				</p>
-			);
-		} else {
-			return (
-				<p>
-					{file.name}.{file.type}
-				</p>
-			);
-		}
-	});
-	return files;
-}
-
-const mappedFiles = getFiles(data);
-
-console.log(data);
-
 function App() {
+	const [files, setFiles] = useState(mapFiles(JSONdata));
+	const [sort, setSort] = useState("");
+	const [sortOrder, setSortOrder] = useState("asc");
+
+	useLayoutEffect(() => {
+		setFiles(mapFiles(sortFiles(JSONdata)));
+	}, [sort, sortOrder]);
+
+	function mapFiles(data) {
+		const mappedFiles = data.map((file) => {
+			if (file.type === "folder") {
+				return (
+					<div className="folder" key={file.name}>
+						./{file.name}
+						<span className="tab">{mapFiles(file.files)}</span>
+					</div>
+				);
+			} else {
+				return (
+					<div key={file.name}>
+						{file.name}.{file.type}
+					</div>
+				);
+			}
+		});
+		return mappedFiles;
+	}
+
+	function handleSortChange(e) {
+		setSort(e.target.value);
+	}
+
+	function handleSortOrderChange(e) {
+		setSortOrder(e.target.value);
+	}
+
+	function sortFiles(data) {
+		const sortedFiles = data.sort((a, b) => {
+			if (sort === "name") {
+				return a.name.localeCompare(b.name);
+			} else if (sort === "size") {
+				return a.size - b.size;
+			} else if (sort === "date") {
+				return new Date(a.added) - new Date(b.added);
+			}
+		});
+		if (sortOrder === "desc") {
+			sortedFiles.reverse();
+		}
+		return sortedFiles;
+	}
+
 	return (
 		<div className="App">
-			<h1>Bright HR</h1>
-			<select name="select" id="select">
-				<option value="all">All</option>
-				<option value="active">Active</option>
-				<option value="inactive">Inactive</option>
+			<h1>Bright HR{sort}</h1>
+			<select name="sort" onChange={handleSortChange}>
+				<option value="">Sort by</option>
+				<option value="name">Name</option>
+				<option value="size">Size</option>
+				<option value="date">Date</option>
 			</select>
-			<pre>{mappedFiles}</pre>
+			<select name="sortOrder" onChange={handleSortOrderChange}>
+				<option value="">Sort Order</option>
+				<option value="asc">Ascending</option>
+				<option value="desc">Descending</option>
+			</select>
+			<pre>{files}</pre>
 		</div>
 	);
 }
