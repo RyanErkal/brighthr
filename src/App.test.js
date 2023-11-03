871a5218c2e8f2288dc9a3d87298c2d33d2f53d9
Ryan Erkal <ryanerkal@yahoo.ie>
2023-11-03 13:39:41 +0000

    basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,18 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("List renders successfully", () => {
+test("List item format correct", () => {
 	render(<App />);
 
-	const element = screen.getByText(/Public Holiday/);
+	const element = screen.getByText(/Public Holiday policy.pdf - 15kB/);
+
+	expect(element).toBeInTheDocument();
+});
+
+test("List folder format correct", () => {
+	render(<App />);
+
+	const element = screen.getByText(/.\/Expenses - 20kB/);
 
 	expect(element).toBeInTheDocument();
 });
